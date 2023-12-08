8ab9ee050a2cae4511e211c6ce12e604acfbf224
Danka József Dániel <dankajdx@gmail.com>
2023-12-08 18:14:46 +0100

    adding more config

--- theater/src/main/java/com/example/theater/config/SecurityConfiguration.java
+++ theater/src/main/java/com/example/theater/config/SecurityConfiguration.java
@@ -4,6 +4,7 @@ import com.example.theater.services.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
@@ -33,6 +34,7 @@ public class SecurityConfiguration {
                 .sessionManagement(manager -> manager.sessionCreationPolicy(STATELESS))
                 .authenticationProvider(authenticationProvider()).addFilterBefore(
                         jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+
         return http.build();
     }
 
